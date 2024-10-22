fd0b572140643fccac7887e6d25fb96b9d83f02a
Pooja <221296@tkmce.ac.in>
2024-10-22 18:05:08 +0000

    Verified Properties of DFT

---
+++ Experiment 7- Properties of DFT.m
@@ -0,0 +1,92 @@
+clc;               % linearity prop
+clear all;
+close all;
+x=input("enter first sequence");
+h=input("enter sequence sequence:");
+lx=length(x);
+lh=length(h);
+if lx>lh
+    h=[h zeros(1,lx-lh)]
+else
+x=[x zeros(1,lh-lx)]
+end
+a=input("enter value of 'a':");
+b=input("enter value of 'b':");
+lhs=fft((a.*x)+(b.*h));
+rhs=a.*fft(x)+b.*fft(h);
+disp('LHS');
+disp(lhs);
+disp('RHS');
+disp(rhs);
+if lhs==rhs
+disp('Linearity property verified');
+else
+disp('Linearity property not verified');
+end
+
+
+clc;                 % parsevals prop
+clear all;
+close all;
+x=input("enter first sequence:");
+h=input("enter second sequence:");
+N=max(length(x),length(h));
+xn=[x zeros(1,N-length(x))];
+hn=[h zeros(1,N-length(h))];
+lhs=sum(xn.*conj(hn));
+rhs=sum(fft(xn).*conj(fft(hn)))/N;
+disp('LHS');
+disp(lhs);
+disp('RHS');
+disp(rhs);
+if lhs==rhs
+disp("Parseval's Theorem verified");
+else
+disp("Parseval's Theorem not verified");
+end
+
+
+clc;                % convolution prop
+clear all;
+close all;
+x=input("enter first sequence");
+h=input("enter sequence sequence:");
+N=max(length(x), length(h));
+xn=[x zeros(N-length(x))];
+hn=[h zeros(N-length(h))];
+Xn=fft(xn);
+Hn=fft(hn);
+lhs=cconv(xn,hn,N);
+rhs=ifft(Xn.*Hn);
+disp('LHS');
+disp(lhs);
+disp('RHS');
+disp(rhs);
+if lhs==rhs
+disp('Circular Convolution verified')
+else
+disp('Circular Convolution not verified');
+end
+
+
+clc;                 % multiplication prop
+clear all;
+close all;
+x=input("enter first sequence");
+h=input("enter sequence sequence:");
+N=max(length(x), length(h));
+xn=[x zeros(N-length(x))];
+hn=[h zeros(N-length(h))];
+lhs=fft(xn.*hn);
+Xn=fft(xn);
+Hn=fft(hn);
+rhs=(cconv(Xn,Hn,N))/N;
+disp('LHS');
+disp(lhs);
+disp('RHS');
+disp(rhs);
+if lhs==rhs
+disp('Multiplication property verified');
+else
+disp('Multiplication property not verified');
+end
